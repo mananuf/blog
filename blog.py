3bbf7bcca77d13c440f1f94c7361b6fb94f24553
mananuf <bmananaf@gmail.com>
2022-02-21 06:12:24 +0100

    installed flask-wtf forms

--- blog.py
+++ blog.py
@@ -3,9 +3,30 @@ from flask import Flask,render_template,url_for
 app = Flask(__name__)
 
 
+posts = [ #an array/list of dictionaries, containing blog post
+    {
+        'title': 'Blog Post 1',
+        'author': 'Bankat Man',
+        'content': 'first post content',
+        'date': '12th February, 2022'
+    },
+    {
+        'title': 'Blog Post 2',
+        'author': 'Elizabeth creow',
+        'content': 'second post content',
+        'date': '18th February, 2022'
+    },
+    {
+        'title': 'Blog Post 3',
+        'author': 'Mahnjong seo',
+        'content': 'third post content',
+        'date': '21st February, 2022'
+    }
+]
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", posts = posts)
 
 
 @app.route("/about")
