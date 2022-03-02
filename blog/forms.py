511926d73e9c7f1a7059787454ab0fdbbcc237ca
mananuf <bmananaf@gmail.com>
2022-03-02 05:16:58 +0100

    added validation for username and email to check if it already exist

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,3 @@
-from distutils.command.config import config
-from xml.dom import ValidationErr
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
