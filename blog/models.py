654c7ae97db8de12b76aa46696710259778d4d90
mananuf <bmananaf@gmail.com>
2022-03-02 05:18:45 +0100

    imported login_manager from blog and created an extension for user_loader

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,15 @@
 from datetime import datetime
-from blog import db
+from re import U
+from blog import db, login_manager
+from flask_login import UserMixin
 
-class User(db.Model):
+
+# extension that gets user_id
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     username = db.Column(db.String(20), unique=True, nullable=False) 
     email = db.Column(db.String(120), unique=True, nullable=False)
